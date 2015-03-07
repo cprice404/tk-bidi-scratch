34e40fcd55d3c90c1743463ae9f6c92c358f1655
Chris Price <chris@puppetlabs.com>
2015-03-07 11:44:58 -0800

    WIP: cleanup

--- src/puppetlabs/bidi_utils.clj
+++ src/puppetlabs/bidi_utils.clj
@@ -3,7 +3,7 @@
             [clojure.string :as str]
             [clojure.zip :as zip]))
 
-(defn maps-to-vectors*
+#_(defn maps-to-vectors*
   [l]
   (cond
     (zip/end? l)
@@ -15,38 +15,9 @@
     :else
     (maps-to-vectors* (zip/next l))))
 
-(defn maps-to-vectors
+#_(defn maps-to-vectors
   [routes]
   (-> routes zip/vector-zip maps-to-vectors*))
-
-#_(defn update-route-info
-  [route-info pattern]
-  (cond
-    (contains? #{:get :post :put :delete :head} pattern)
-    (assoc-in route-info [:method] (-> pattern name str/upper-case))
-
-    :else
-    (update-in route-info [:path] str
-               (if (vector? pattern)
-                 (str/join pattern)
-                 pattern))))
-
-#_(defn print-routes
-  ([routepair]
-   (print-routes {:path "" :method :any} routepair))
-  ([route-info [pattern matched]]
-   (let [route-info (update-route-info route-info pattern)]
-     (if (vector? matched)
-       (doseq [routepair matched]
-         (print-routes route-info routepair))
-       (println (str (:method route-info) ": " (:path route-info)))))))
-
-#_(defn route-metadata*
-  [acc ])
-
-#_(defn nested-routes*
-  [routes route-info pattern matched]
-  )
 
 (defn update-route-info
   [route-info pattern]
@@ -61,85 +32,48 @@
                  [pattern]))))
 
 (defn route-metadata*
-  [depth routes route-info loc]
+  [routes route-info loc]
   (if (nil? loc)
     routes
     (let [node (zip/node loc)]
-      (println depth "LOC node:" node)
       (cond
         (map? node)
         (do
-          (println depth "FOUND MAP AT ROOT LEVEL")
-          (route-metadata* (inc depth) routes route-info (zip/edit loc #(into [] %))))
+          (route-metadata* routes route-info (zip/edit loc #(into [] %))))
 
         (vector? node)
         (let [[pattern matched] (zip/node loc)]
-          (println depth "PATTERN:" pattern)
           (let [routes (cond
                          (map? matched)
                          (do
-                           (println depth "Found a map for MATCHED, converting")
-                           (route-metadata* (inc depth)
-                                            routes route-info
-                                            #_(-> loc
-                                                zip/down
-                                                zip/right
-                                                (zip/edit #(into [] %))
-                                                zip/up)
+                           (route-metadata* routes route-info
                                             (zip/vector-zip [pattern (into [] matched)])))
 
                          (vector? matched)
                          (do
-                           (println depth "MATCHED VECTOR:" matched)
-                           (println depth "\tDOWN:" (-> loc zip/down zip/right zip/down zip/node))
                            (route-metadata*
-                             (inc depth)
                              routes
                              (update-route-info route-info pattern)
                              (-> loc zip/down zip/right zip/down)))
 
                          :else
                          (do
-                           (println depth "\nADDING ROUTE:"
-                                    pattern "|"
-                                    matched "|"
-                                    (update-route-info route-info pattern))
-                           (conj routes (update-route-info route-info pattern))
-                           #_(route-metadata* routes route-info (zip/next loc))))]
-            (println depth "Routes is now:" routes)
+                           (conj routes (update-route-info route-info pattern))))]
             (let [next (zip/right loc)]
-              (println depth "Moving to next node:")
-              (if (nil? next)
-                (println depth "nil")
-                (println depth (zip/node next)))
               (route-metadata*
-                (inc depth)
                 routes
                 route-info
                 next))))
 
         :else
-        #_(route-metadata* routes route-info (zip/next loc))
-        #_(let [[pattern matched] (zip/node loc)]
-          (println "PATTERN:" pattern)
-          (if (vector? matched)
-            (do
-              (println "MATCHED VECTOR:" matched)
-              (println "\tDOWN:" (-> loc zip/down zip/next zip/node))
-              (route-metadata* routes route-info (zip/next loc)))
-            (do
-              (println "MATCHED IS NOT A VECTOR:" matched)
-              (route-metadata* routes route-info (zip/next loc)))))
-        (throw (IllegalStateException. "d'oh"))
-        ))))
+        (throw (IllegalStateException. "d'oh"))))))
 
 (defn route-metadata
   [routes]
   (->> [routes]
        zip/vector-zip
        zip/down
-       (route-metadata* 0
-                        []
+       (route-metadata* []
                         {:path []
                          :method :any})))
 
@@ -150,11 +84,6 @@
 
 (defn routes->handler
   [routes]
-  #_(let [vector-routes (maps-to-vectors routes)]
-    (with-meta
-      (bidi-ring/make-handler
-        vector-routes)
-      {:routes (route-metadata vector-routes)}))
   (with-meta
     (bidi-ring/make-handler routes)
     {:routes (route-metadata routes)}))
